4972f419b4f7a849eda7531f030247a9015afe51
VasilyDrozhzhin <vasilydrozhzhin@gmail.com>
2024-12-17 00:37:14 +0300

    Add files via upload

--- Module_9_6.py
+++ Module_9_6.py
@@ -1,7 +1,9 @@
+# https://github.com/VasilyDrozhzhin/Module_9/blob/main/Module_9_6.py
 def  all_variants(text):
 	length = len(text)
-	for start in range(length):
-		for end in range(start + 1, length + 1):
+	for sub_length in range(1, length + 1):
+		for start in range(length - sub_length + 1):
+			end = start + sub_length
 			yield text[start:end]
 
 
